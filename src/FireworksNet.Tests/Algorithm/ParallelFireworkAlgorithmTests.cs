5330b734e7262a40526f998ded6d7511f76c09bb
sleshJdev <slesh.eugene93@gmail.com>
2015-05-04 04:14:40 +0200

    Add basic test for ParallelFireworkAlgorithmTests.

--- src/FireworksNet.Tests/Algorithm/ParallelFireworkAlgorithmTests.cs
+++ src/FireworksNet.Tests/Algorithm/ParallelFireworkAlgorithmTests.cs
@@ -8,6 +8,16 @@ namespace FireworksNet.Tests.Algorithm
 {
     public class ParallelFireworkAlgorithmTests : AlgorithmTestDataSource
     {
+        [Theory]
+        [MemberData("DataForTestingCreationOfParallelFireworkAlgorithm")]
+        public void CreationParallelFireworkAlgorithm_PassEachParameterAsNullAndOtherIsCorrect_ArgumentNullExceptionThrown(
+            Problem problem, IStopCondition stopCondition, ParallelFireworksAlgorithmSettings settings, string expectedParamName)
+        {
+            ArgumentException exception = Assert.Throws<ArgumentNullException>(() => new ParallelFireworksAlgorithm(problem, stopCondition, settings));
+
+            Assert.Equal(expectedParamName, exception.ParamName);
+        }
+
         
     }
 }
